e1e27991cfb84cebebf53002f30031b2454fbc48
GowthamNagaraj <84234435+GowthamNagaraj@users.noreply.github.com>
2025-04-25 14:38:11 +0530

    Aadhar form created

---
+++ Frontend/Aadhar/src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import AadharForm from './Components/Form/AadharForm'
+import AadharTable from './Components/Table/AadharTable'
+
+function App() {
+  
+  return (
+    <>
+      <Routes>
+        <Route path="/AadharForm" element={<AadharForm />}/>
+        <Route path="/AadharTable" element={<AadharTable />}/>
+      </Routes>      
+    </>
+  )
+}
+
+export default App
